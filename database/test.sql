29c4432c5113d409ac0b10403957c2275bdf32c0
7laurent <zepierre180@gmail.com>
2026-08-26 15:50:31 +0100

    Initial commit

---
+++ database/test.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO equipment
+(equipment_code, equipment_type, brand, model)
+VALUES
+('VEH-001', 'Camion', 'Volvo', 'FH16'),
+('VEH-002', 'Camion', 'Scania', 'R450'),
+('VEH-003', 'Engin', 'Caterpillar', '320D');
+
+SELECT * FROM equipment;
